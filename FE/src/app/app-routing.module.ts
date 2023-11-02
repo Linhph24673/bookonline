924b5c2ec45cff11378e4587c76dd04ae48f214d
linhnguyen <linhnguyen13092003@gmail.com>
2023-11-02 12:08:09 +0700

    2/11

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './component/pages/home/homepage/homepage.component';
+import { AdminlayoutComponent } from './component/layout/adminlayout/adminlayout/adminlayout.component';
+import { DashboardComponent } from './component/pages/admin/dashboard/dashboard.component';
+import { ManagerProductComponent } from './component/pages/admin/manager-product/manager-product.component';
+import { AddProductComponent } from './component/pages/admin/add-product/add-product.component';
+import { UpdateProductComponent } from './component/pages/admin/update-product/update-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:HomepageComponent},
+  {path:'admin',component:AdminlayoutComponent,children:[
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: ManagerProductComponent },
+      { path: 'add', component: AddProductComponent },
+      { path: 'products/:id/edit', component: UpdateProductComponent },
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './component/pages/admin/add-product/add-product.component';
+import { ManagerProductComponent } from './component/pages/admin/manager-product/manager-product.component';
+import { UpdateProductComponent } from './component/pages/admin/update-product/update-product.component';
+import { DashboardComponent } from './component/pages/admin/dashboard/dashboard.component';
+import { HomepageComponent } from './component/pages/home/homepage/homepage.component';
+import { AdminlayoutComponent } from './component/layout/adminlayout/adminlayout/adminlayout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ManagerProductComponent,
+    UpdateProductComponent,
+    DashboardComponent,
+    HomepageComponent,
+    AdminlayoutComponent
   ],
   imports: [
     BrowserModule,
